e673c966aea97115291440e5e02968f9a33086f5
Cayque Santos <cayque_santos16@hotmail.com>
2020-04-26 13:30:48 -0300

    Adicionado a escrita do resultado em um arquivo

--- datas_de_leitura.py
+++ datas_de_leitura.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import date,timedelta
 
+livro = input("Informe o nome do livro: ")
 dia,mes,ano = map(int,input("Informe a data de inicio da leitura: ").split("/"))
 dataInicial = date(ano,mes,dia)
 porcent = 5
@@ -14,4 +15,9 @@ for i in range(1,21):
     result += " | "
   dataInicial += timedelta(days=1)
   porcent += 5
+
+arquivo = open(livro+'.txt','w')
+arquivo.write(result)
+arquivo.close()
+
 print(result)
