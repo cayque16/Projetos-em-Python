692f2891b97284c79505f575b0652065a61d0db7
Cayque Santos <cayque_santos16@hotmail.com>
2020-04-29 10:43:25 -0300

    Exemplo de api com flask.

---
+++ api-com-flask/app/models/users.py
@@ -0,0 +1,25 @@
+import datetime
+from app import db, ma
+
+"""Definicao da classe/tabela dos usuarios e seus campos"""
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	password = db.Column(db.String(200), nullable=False)
+	name = db.Column(db.String(60), nullable=False)
+	email = db.Column(db.String(50), unique=True, nullable=False)
+	created_on = db.Column(db.DateTime, default=datetime.datetime.now())
+
+	def __init__(self, username, password, name, email):
+		self.username = username
+		self.password = password
+		self.name = name
+		self.email = email
+
+"""Definindo o Schema do Marshmallow para facilitar a utilizacao de JSON"""
+class UsersSchema(ma.Schema):
+	class Meta:
+		fields = ('id', 'username', 'name', 'email', 'password', 'created_on')
+
+user_schema = UsersSchema()
+users_schema = UsersSchema(many=True)
