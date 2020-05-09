088073d62f491879856d1635842ade3d4291257e
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-09 14:34:42 -0300

    Adicionando explicacao dos argumentos da funcao.

--- concatena_zeros.py
+++ concatena_zeros.py
@@ -1,5 +1,8 @@
 def conacatena_zeros(texto: str,tam_final: int,esquerda=True) -> str: 
     # Concatena zeros a uma determinada string
+    #   texto -> texto que se deseja concatenar os 0s
+    #   tam_final -> tamanho final que o texto deve ter
+    #   esquerda -> onde se deve concatenar os 0s esquerda(True) ou direita(False)
     tam_final -= len(texto)
     if tam_final > 0:
         while tam_final > 0:
