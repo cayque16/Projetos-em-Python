7d25c73ef90867e033076e245daddf218b7ec89f
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-25 10:26:19 -0300

    Iniciando o projeto para o curso de django. Inserido o codigo da primeira aula, alem do exercicio proposta no final da aula.

---
+++ curso-django/hello_django/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def hello(request, nome):
+    return HttpResponse("<h1>Hello {}!!!</h1>".format(nome))
+
+def soma(request, num1, num2):
+    soma = num1 + num2
+    return HttpResponse("{} + {} = {}".format(num1,num2,soma))
