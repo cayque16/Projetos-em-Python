b889458aba163af2d4464031b4ccafe849d1e336
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-26 10:33:37 -0300

    Incluido template para a insercao de eventos da agenda. Incluido o gitignore para ignorar arquivos compilados.

--- curso-django/agenda/agenda/urls.py
+++ curso-django/agenda/agenda/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', views.index),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user),
