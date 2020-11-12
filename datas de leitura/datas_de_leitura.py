d032e85d0e0405fd713a99997f604546b440e3ee
Cayque Santos <cayque_santos16@hotmail.com>
2020-11-12 12:25:11 -0300

    Criando opcao de gerar planos de leitura com base no numero de paginas.

--- datas de leitura/datas_de_leitura.py
+++ datas de leitura/datas_de_leitura.py
@@ -2,19 +2,42 @@
 from datetime import date,timedelta
 
 livro = input("Informe o nome do livro: ")
+tipo = int(input("Percentual(1) ou paginas(2)? "))
+if (tipo == 2):
+  paginas = int(input("Informe o numero de paginas: "))
+
 dia,mes,ano = map(int,input("Informe a data de inicio da leitura: ").split("/"))
 dataInicial = date(ano,mes,dia)
-porcent = 5
+resto = 0
+if (tipo != 2):
+  porcent = 5
+  simbolo = '%'
+else:
+  porcent = paginas // 20
+  resto = paginas % 20
+  simbolo = ''
 result = ""
+total = 0
 
 for i in range(1,21):
-  result += "{}: {}%".format(dataInicial.strftime('%d/%m/%Y'),"05" if porcent == 5 else porcent)
+  temp = porcent
+  if(resto > 0 and tipo == 2):
+    porcent += 1
+    resto -= 1
+  if(tipo != 2):
+    total = porcent
+  else:
+    total += porcent
+  valor = "0"+str(total) if total < 10 else total
+  porcent = temp
+  result += "{}: {}{}".format(dataInicial.strftime('%d/%m/%Y'),valor,simbolo)
   if(i%4==0):
     result += "\n"
   else:
     result += " | "
   dataInicial += timedelta(days=1)
-  porcent += 5
+  if(tipo != 2):
+    porcent += 5
 
 livro = "/var/www/Projetos-em-Python/datas de leitura/livros/" + livro.replace(" ","_")
 arquivo = open(livro+'.txt','w')
