8a6a10e31f56a27975b7a3ffe81c1a10866ad709
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-09 21:51:05 -0300

    feature: adiciona os livros ao historico geral.

--- datas-de-leitura/datas_de_leitura.py
+++ datas-de-leitura/datas_de_leitura.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from datetime import date,timedelta
 import os.path
+import json
 
 livro = input("Informe o nome do livro: ")
 tipo = int(input('''Informe a forma de marcação:
   1 - Percentual
   2 - Páginas
 '''))
+paginas = 0
 if (tipo == 2):
   paginas = int(input("Informe o numero de paginas: "))
 
@@ -15,6 +17,7 @@ dia,mes,ano = map(int,input("Informe a data de inicio da leitura: ").split("/"))
 if ano < 2000:
   ano = '20'+str(ano)
 dataInicial = date(int(ano),mes,dia)
+startDate = str(ano) + '-' + str(mes).zfill(2) + '-' + str(dia)
 
 resto = 0
 if (tipo != 2):
@@ -57,9 +60,32 @@ if os.path.isdir(pasta):
 else:
   os.mkdir(pasta)
 
-livro = pasta+"/"+str(indice).zfill(2)+'_'+livro.replace(" ","_")
-arquivo = open(livro+'.txt','w')
+livro_arq = pasta+"/"+str(indice).zfill(2)+'_'+livro.replace(" ","_")
+arquivo = open(livro_arq+'.txt','w')
 arquivo.write(result)
 arquivo.close()
+
+arquivo_historico = 'livros/historico-geral.json'
+with open(arquivo_historico, 'r') as arquivo:
+  dados = json.load(arquivo)
+
+novo_livro = {
+  "title": livro,
+  "startDate": startDate,
+  "totalPage": paginas
+}
+
+if (int(ano) == (dados['data'][-1]['year'])):
+  dados['data'][-1]['books'].append(novo_livro)
+else:
+  novo_ano = {
+    "year": int(ano),
+    "books": [novo_livro]
+  }
+
+  dados['data'].append(novo_ano)
+
+with open(arquivo_historico, 'w') as arquivo:
+  json.dump(dados, arquivo, indent=2)
 
 print(result)
